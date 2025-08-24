c0ca21030940136c9b5062d7e75d910101d0f41a
abcfounder123 <156555912+abcfounder123@users.noreply.github.com>
2025-08-25 05:29:50 +0700

    Create oop_steps_1_to_10.py

---
+++ oop_steps_1_to_10.py
@@ -0,0 +1,77 @@
+"""
+
+Step.1   --->   Write
+
+Car မှာ ကားနံပါတ် တာယာနဲ့ အင်ဂျင်ပါတယ်။ (VIN, engine, tires)
+
+Tires ( ကားတာယာ ) တွင် size နှင့် pressure ပါသည်။
+pressure ၏ မူလတန်ဖိုးသည် 0 ( psi ) ဖြစ်သည်။
+လေထိုးသောလုပ်ဆောင်ချက်ပါသည်။ သတ်မှတ်ပေးလိုက်သော ဖိအားအတိုင်း လေထိုးပေးမည်။
+
+Engine တွင် fuel_type ပါသည်။
+စက်နှိုး/မနှိုး ဟူသော အခြေအနေ  state ပါသည်။
+မူလအခြေအနေမှာ စက်မနှိုးထားသဖြင့် off ဖြစ်နေမည်။
+ပေးထားသော fuel_type ဖြင့် စက်နှိုး ၊ စက်ရပ် မည့်လုပ်‌ဆောင်ချက်ပါသည်။ ( on(), off() )
+
+ဖန်တီးခဲ့သော တာယာနှင့် အင်ဂျင်ကို ယူသုံးပြီး city_car တစ်စီးဖန်တီးပါ။
+ကားနံပါတ်မှာ 001A ၊ တာယာမှာ 15 လက်မ၊ ဆီအမျိုးအစားမှာ petrol ဓါတ်ဆီ ဖြစ်သည်။
+
+ထိုကားကို လေဖိအား 3 psi ထိလေထိုးပြီး စက်နှိုးပါ။
+
+#################################################
+
+Step.2   --->   Divide
+
+class   --->   Car
+data    --->   VIN, tires, engine
+method  --->
+
+class   --->   Tires
+data    --->   size, pressure = 0 ( psi )
+method  --->   pump(p)
+
+class   --->   Engine
+data    --->   fuel_type, state = "off"
+method  --->   on(), off()
+
+#################################################
+
+Step.3   --->   Draw
+
+
+class Car:
+    def __init__(obj, VIN, tires, engine):
+        obj.VIN = VIN
+        obj.tires = tires
+        obj.engine = engine
+
+
+class Tires:
+    def __init__(obj, size):
+        obj.size = size
+        obj.pressure = 0
+
+    def pump(obj, p):
+        print(f"pump to {p} psi.")
+
+
+class Engine:
+    def __init__(obj, fuel_type):
+        obj.fuel_type = fuel_type
+        obj.state = "off"
+
+    def on(obj):
+        print(f"{obj.fuel_type} engine On.")
+
+    def off(obj):
+        pass
+
+
+#################################################
+
+
+
+
+
+
+"""
